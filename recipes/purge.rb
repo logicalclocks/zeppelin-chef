aee3b3929f987849cf1a5fa1354a1f2d0d5afb40
Jim Dowling <jdowling@sics.se>
2015-10-18 00:22:07 +0200

    purge recipe for zeppelin

---
+++ recipes/purge.rb
@@ -0,0 +1,21 @@
+bash 'kill_running_interpreters' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      pkillall -9 Interpreter
+    EOF
+end
+
+
+directory node[:zeppelin][:home] do
+  recursive true
+  action :delete
+  ignore_failure :true
+end
+
+link node[:zeppelin][:base_dir] do
+  action :delete
+  ignore_failure :true
+end
+
+
