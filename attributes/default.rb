46adcc8e21d288e6a3f103664e7b711f75504c43
Jim Dowling <jdowling@sics.se>
2016-01-14 00:58:30 +0100

    upgrade to 0.6-SNAPSHOT

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,9 +4,9 @@ include_attribute "hadoop"
 include_attribute "spark"
 include_attribute "flink"
 
-default[:zeppelin][:version]             = "0.5.0-incubating"
-default[:zeppelin][:spark_version]       = "1.3.1"
-default[:zeppelin][:hadoop_version]      = "2.3"
+default[:zeppelin][:version]             = "0.6.0-incubating-SNAPSHOT"
+default[:zeppelin][:spark_version]       = "1.5.2"
+default[:zeppelin][:hadoop_version]      = "2.4"
 default[:zeppelin][:name]                = "zeppelin-#{node[:zeppelin][:version]}-bin-spark-#{node[:zeppelin][:spark_version]}_hadoop-#{node[:zeppelin][:hadoop_version]}"
 default[:zeppelin][:user]                = node[:hadoop][:yarn][:user]
 default[:zeppelin][:group]               = node[:hadoop][:group]
@@ -15,7 +15,7 @@ default[:zeppelin][:dir]                 = "/srv"
 default[:zeppelin][:base_dir]            = node[:zeppelin][:dir] + "/zeppelin" 
 default[:zeppelin][:home]                = node[:zeppelin][:dir] + "/zeppelin-" + node[:zeppelin][:version]
 
-default[:zeppelin][:url]                 = "#{node[:download_url]}/#{node[:zeppelin][:name]}.tgz"
+default[:zeppelin][:url]                 = "#{node[:download_url]}/#{node[:zeppelin][:name]}.tar.gz"
 
 default[:zeppelin][:executor_memory]     = "512m"
 default[:zeppelin][:driver_memory]       = "1g"
