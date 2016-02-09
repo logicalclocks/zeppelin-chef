3c057bec4f2931ce651450e6251b2169ce661ab9
%github_username% <jdowling@sics.se>
2016-02-09 13:26:43 +0100

    Fixed type bug in purge recipe

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -1,6 +1,6 @@
 bash 'kill_running_interpreters' do
     user "root"
-    ignore_failure :true
+    ignore_failure true
     code <<-EOF
       pkillall -9 Interpreter
     EOF
@@ -10,12 +10,12 @@ end
 directory node[:zeppelin][:home] do
   recursive true
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
 link node[:zeppelin][:base_dir] do
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
 
