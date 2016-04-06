942995cce25359dc265a2bdde31b8a61d7088616
Jim Dowling <jdowling@sics.se>
2016-04-06 15:48:48 -0700

    add source_url to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          "Apache v2"
 description      'Installs/Configures Zeppelin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
+source_url       "https://github.com/hopshadoop/zeppelin-chef"
+
 
 
 depends          "flink"
