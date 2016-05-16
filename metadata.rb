4f605603aa5eb98375efd84df8be2baabf9b0e5b
Jim Dowling <jdowling@sics.se>
2016-05-17 00:34:24 +0200

    Adding livy params to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -17,6 +17,7 @@ depends          "kagent"
 
 recipe           "install", "Installs Zeppelin binaries"
 recipe           "default", "Starts a Zeppelin master"
+recipe           "livy",    "Installs a Livy Spark REST Server"
 
 attribute "zeppelin/user",
           :description => "User to install/run zeppelin as",
@@ -26,3 +27,26 @@ attribute "zeppelin/dir",
           :description => "zeppelin base dir",
           :type => 'string'
 
+attribute "livy.version",
+          :dscription => "livy.version",
+          :type => "string"
+
+attribute "livy.url",
+          :dscription => "livy.url",
+          :type => "string"
+
+attribute "livy.port",
+          :dscription => "livy.port",
+          :type => "string"
+
+attribute "livy.home",
+          :dscription => "livy.home",
+          :type => "string"
+
+attribute "ivy.keystore",
+          :dscription => "ivy.keystore",
+          :type => "string"
+
+attribute "ivy.keystore.password",
+          :dscription => "ivy.keystore.password",
+          :type => "string"
