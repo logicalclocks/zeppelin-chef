9a7fb8fe674f01a998c1f0c2a0248b1d24e718d1
Jim Dowling <jdowling@sics.se>
2016-05-19 08:05:12 +0200

    livy cleanup

--- recipes/install.rb
+++ recipes/install.rb
@@ -108,43 +108,4 @@ end
 
 
 
-##### Livy Installation
-
-
-package_url = "#{node.livy.url}"
-base_package_filename = File.basename(package_url)
-cached_package_filename = "#{Chef::Config.file_cache_path}/#{base_package_filename}"
-
-remote_file cached_package_filename do
-  source package_url
-  owner "#{node.hadoop_spark.user}"
-  mode "0644"
-  action :create_if_missing
-end
-
-
-package "unzip" do
-end
-
-# Extract Livy
-livy_downloaded = "#{node.livy.dir}/.livy_extracted_#{node.livy.version}"
-
-bash 'extract-livy' do
-        user "root"
-        group node.hadoop_spark.group
-        code <<-EOH
-                set -e
-                unzip #{cached_package_filename} -d #{Chef::Config.file_cache_path}
-                mv #{Chef::Config.file_cache_path}/livy-server-#{node.livy.version} #{node.livy.dir}
-                # remove old symbolic link, if any
-                rm -f #{node.livy.home}
-                ln -s #{node.livy.base_dir} #{node.livy.home}
-                chown -R #{node.hadoop_spark.user}:#{node.hadoop_spark.group} #{node.livy.base_dir}
-                touch #{livy_downloaded}
-                chown -R #{node.hadoop_spark.user}:#{node.hadoop_spark.group} #{livy_downloaded}
-        EOH
-     not_if { ::File.exists?( "#{livy_downloaded}" ) }
-end
-
-
 
