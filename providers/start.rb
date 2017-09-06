26b2d93f00d24a7555492aff79e498f4a34cbf86
Fabio Buso <dev.siroibaf@gmail.com>
2017-09-06 12:06:53 +0200

    [HOPSWORKS-194] Change chef cookbooks from dot notation to bracket notation

--- providers/start.rb
+++ providers/start.rb
@@ -1,10 +1,10 @@
 action :start do
 
   bash "start-zeppelin" do
-    user node.zeppelin.user
-    group node.zeppelin.group
+    user node['zeppelin']['user']
+    group node['zeppelin']['group']
     code <<-EOF
-      cd #{node.zeppelin.home}
+      cd #{node['zeppelin']['home']}
       ./bin/zeppelin-daemon.sh start
     EOF
   end
@@ -14,10 +14,10 @@ end
 action :stop do
 
   bash "stop-zeppelin" do
-    user node.zeppelin.user
-    group node.zeppelin.group
+    user node['zeppelin']['user']
+    group node['zeppelin']['group']
     code <<-EOF
-      cd #{node.zeppelin.home}
+      cd #{node['zeppelin']['home']}
       ./bin/zeppelin-daemon.sh stop
     EOF
   end
--- recipes/install.rb
+++ recipes/install.rb
@@ -9,132 +9,132 @@
 
 include_recipe "java"
 
-group node.zeppelin.group do
+group node['zeppelin']['group'] do
   action :create
-  not_if "getent group #{node.zeppelin.group}"
+  not_if "getent group #{node['zeppelin']['group']}"
 end
 
-user node.zeppelin.user do
+user node['zeppelin']['user'] do
   action :create
-  gid node.zeppelin.group
+  gid node['zeppelin']['group']
   system true
-  home "/home/#{node.zeppelin.user}"  
+  home "/home/#{node['zeppelin']['user']}"
   shell "/bin/bash"
   manage_home true
-  not_if "getent passwd #{node.zeppelin.user}"
+  not_if "getent passwd #{node['zeppelin']['user']}"
 end
 
-group node.zeppelin.group do
+group node['zeppelin']['group'] do
   action :modify
-   members ["#{node.zeppelin.user}"]
+   members ["#{node['zeppelin']['user']}"]
   append true
 end
 
-directory node.zeppelin.dir do
-  owner node.zeppelin.user
-  group node.hops.group
+directory node['zeppelin']['dir'] do
+  owner node['zeppelin']['user']
+  group node['hops']['group']
   mode "0750"
   action :create
-  not_if { File.directory?("#{node.zeppelin.dir}") }
+  not_if { File.directory?("#{node['zeppelin']['dir']}") }
 end
 
 
-package_url = "#{node.zeppelin.url}"
+package_url = "#{node['zeppelin']['url']}"
 base_package_filename = File.basename(package_url)
-cached_package_filename = "#{Chef::Config[:file_cache_path]}/#{base_package_filename}"
+cached_package_filename = "#{Chef::Config['file_cache_path']}/#{base_package_filename}"
 
 remote_file cached_package_filename do
   source package_url
-  checksum node.zeppelin.checksum
-  owner node.zeppelin.user
+  checksum node['zeppelin']['checksum']
+  owner node['zeppelin']['user']
   mode "0644"
   action :create_if_missing
 end
 
-zeppelin_down="#{node.zeppelin.home}/.zeppelin_extracted_#{node.zeppelin.version}"
+zeppelin_down="#{node['zeppelin']['home']}/.zeppelin_extracted_#{node['zeppelin']['version']}"
 # Extract Zeppelin
 bash 'extract-zeppelin' do
         user "root"
-        group node.zeppelin.group
+        group node['zeppelin']['group']
         code <<-EOH
                 set -e
-                cd #{Chef::Config[:file_cache_path]}
-                tar -xf #{cached_package_filename} -C #{Chef::Config[:file_cache_path]}
-                mv #{Chef::Config[:file_cache_path]}/zeppelin-#{node.zeppelin.version} #{node.zeppelin.dir}
-                mkdir -p #{node.zeppelin.home}/run
+                cd #{Chef::Config['file_cache_path']}
+                tar -xf #{cached_package_filename} -C #{Chef::Config['file_cache_path']}
+                mv #{Chef::Config['file_cache_path']}/zeppelin-#{node['zeppelin']['version']} #{node['zeppelin']['dir']}
+                mkdir -p #{node['zeppelin']['home']}/run
                 wget http://snurran.sics.se/hops/zeppelin-interpreter.tgz
                 tar -xf zeppelin-interpreter.tgz
-                mv zeppelin-interpreter #{node.zeppelin.home}
-                chown -R #{node.zeppelin.user}:#{node.hops.group} #{node.zeppelin.home}
-                chmod 770 #{node.zeppelin.home}
+                mv zeppelin-interpreter #{node['zeppelin']['home']}
+                chown -R #{node['zeppelin']['user']}:#{node['hops']['group']} #{node['zeppelin']['home']}
+                chmod 770 #{node['zeppelin']['home']}
                 touch #{zeppelin_down}
         EOH
      not_if { ::File.exists?( "#{zeppelin_down}" ) }
 end
 
 
-link node.zeppelin.base_dir do
-  owner node.zeppelin.user
-  group node.zeppelin.group
-  to node.zeppelin.home
+link node['zeppelin']['base_dir'] do
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
+  to node['zeppelin']['home']
 end
 
 
 my_ip = my_private_ip()
 
-file "#{node.zeppelin.home}/conf/zeppelin-env.sh" do
+file "#{node['zeppelin']['home']}/conf/zeppelin-env.sh" do
  action :delete
 end
 
-template "#{node.zeppelin.home}/conf/zeppelin-env.sh" do
+template "#{node['zeppelin']['home']}/conf/zeppelin-env.sh" do
   source "zeppelin-env.sh.erb"
-  owner node.zeppelin.user
-  group node.zeppelin.group
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
   mode 0655
-  variables({ 
+  variables({
         :private_ip => my_ip,
-        :hadoop_dir => node.hops.base_dir,
-        :spark_dir => node.hadoop_spark.base_dir
+        :hadoop_dir => node['hops']['base_dir'],
+        :spark_dir => node['hadoop_spark']['base_dir']
            })
 end
 
-file "#{node.zeppelin.home}/conf/interpreter.json" do
+file "#{node['zeppelin']['home']}/conf/interpreter.json" do
  action :delete
 end
 
-template "#{node.zeppelin.home}/conf/interpreter.json" do
+template "#{node['zeppelin']['home']}/conf/interpreter.json" do
   source "interpreter.json.erb"
-  owner node.zeppelin.user
-  group node.zeppelin.group
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
   mode 0655
-  variables({ 
-        :hadoop_home => node.hops.base_dir,
-        :spark_home => node.hadoop_spark.base_dir,
-        :zeppelin_home => node.zeppelin.base_dir,
-        :version => node.zeppelin.version
+  variables({
+        :hadoop_home => node['hops']['base_dir'],
+        :spark_home => node['hadoop_spark']['base_dir'],
+        :zeppelin_home => node['zeppelin']['base_dir'],
+        :version => node['zeppelin']['version']
            })
 end
 
-template "#{node.zeppelin.home}/bin/alive.sh" do
+template "#{node['zeppelin']['home']}/bin/alive.sh" do
   source "alive.sh.erb"
-  owner node.zeppelin.user  
-  group node.zeppelin.group
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
   mode 0755
-  variables({ 
+  variables({
            })
 end
 
-directory "#{node.zeppelin.home}/run" do
-  owner node.zeppelin.user
-  group node.zeppelin.group
+directory "#{node['zeppelin']['home']}/run" do
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
   mode 0655
   mode "755"
   action :create
 end
 
-directory "#{node.zeppelin.home}/logs" do
-  owner node.zeppelin.user
-  group node.zeppelin.group
+directory "#{node['zeppelin']['home']}/logs" do
+  owner node['zeppelin']['user']
+  group node['zeppelin']['group']
   mode 0655
   mode "755"
   action :create
@@ -143,7 +143,7 @@ end
 
 
 # Support for 'R' in apache zeppelin
-case node.platform_family
+case node['platform_family']
 when "debian"
  package "r-base" do
   action :install
